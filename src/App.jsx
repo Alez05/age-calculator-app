7494a82e4290adaf846a37fb1852dd300e01a173
alez05 <cercel.alexandru436@gmail.com>
2024-03-03 23:52:05 +0000

    fonts and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { Container } from './style';
-
 const MyApp = () => {
   return (
-    <Container className='ma-ta'>
-      <h1>My App</h1>
-    </Container>
-  );
+    <h1>My App</h1>
+    )
 };
 
 export { MyApp };
